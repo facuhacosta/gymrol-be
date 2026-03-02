066d49e2255c2844c912321b7a86f61170f06cf5
facuhacosta <facuacosta200997@hotmail.com>
2026-03-02 14:44:31 -0300

    added tokens

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
-    accountId: "YOUR_ACCOUNT_ID", // Placeholder
-    databaseId: "gymrol-db-id",
-    token: "YOUR_TOKEN", // Placeholder
+    accountId: "3375c7006881c3b666bce6db4ea0b168", // Placeholder
+    databaseId: "22a1442c-64be-466b-917c-02e3a19c5d9c",
+    token: "TRaNQMdux5_B7YhQH3onOprGQDpJVqNR6AB6RmrK", // Placeholder
   },
 });
